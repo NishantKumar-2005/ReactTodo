e3326613d8a660a81f97a7283a3c4591c65bd7f0
NishantKumar-2005 <120775351+NishantKumar-2005@users.noreply.github.com>
2023-09-14 20:05:54 +0530

    Updated

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,15 @@
 
 import { useContext } from 'react'
 import { Context } from '../main'
+import { Navigate } from 'react-router-dom';
 
 const Profile = () => {
     const {user} = useContext(Context);
-    console.log(user);
+    const {isAthenticated}=useContext(Context);
+
+    if(!isAthenticated){
+      return <Navigate to={"/loging"}/>
+    }
   return (
     <div>
         <h1>{user?.name}</h1>
